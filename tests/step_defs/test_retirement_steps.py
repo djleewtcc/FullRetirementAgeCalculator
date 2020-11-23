90cd4b26749c91d2250e6447b831b4f0016a39ef
Donald Lee <djlee@my.waketech.edu>
2020-11-22 20:50:56 -0500

    Add int wrappers

--- tests/step_defs/test_retirement_steps.py
+++ tests/step_defs/test_retirement_steps.py
@@ -19,15 +19,15 @@ scenarios('../features/retirement.feature', example_converters=CONVERTERS)
 
 @given('the birth year is set to "<birth_year>"')
 def retire_age(birth_year):
-    return FullRetirementAgeCalculator(birth_year=birth_year)
+    return FullRetirementAgeCalculator(birth_year=int(birth_year))
 
 
 @when('the birth month is "<birth_month>"')
 def set_year(retire_age, birth_month):
-    retire_age.set_birth_month(birth_month)
+    retire_age.set_birth_month(int(birth_month))
 
 
 @then('the retirement year is "<retirement_year>"')
 def check_retirement_year(retire_age, retirement_year):
     retire_age.calculate_retirement_age()
-    assert retire_age.get_retirement_year() == retirement_year
+    assert int(retire_age.get_retirement_year()) == int(retirement_year)
