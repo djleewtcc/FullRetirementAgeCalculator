f2595d98cfaf99bf5fabb2b5d07ae02a956acd19
Donald Lee <djlee@my.waketech.edu>
2020-11-24 20:53:06 -0500

    Added pytest fixture and updated .travis.yml file.

--- tests/step_defs/test_retirement_steps.py
+++ tests/step_defs/test_retirement_steps.py
@@ -16,6 +16,7 @@ CONVERTERS = {
 scenarios('../features/retirement.feature', example_converters=CONVERTERS)
 
 
+@pytest.fixture()
 @given('the birth year is set to "<birth_year>"')
 def retire_age(birth_year):
     return FullRetirementAgeCalculator(birth_year=birth_year)
@@ -23,11 +24,11 @@ def retire_age(birth_year):
 
 @when('the birth month is "<birth_month>"')
 def set_year(retire_age, birth_month):
-    retire_age.set_birth_month(int(birth_month))
+    retire_age.set_birth_month(birth_month)
 
 
 @then('the retirement year is "<retirement_year>"')
 def check_retirement_year(retire_age, retirement_year):
     retire_age.calculate_retirement_age()
-    assert int(retire_age.get_retirement_year()) == int(retirement_year)
+    assert retire_age.get_retirement_year() == retirement_year
 
